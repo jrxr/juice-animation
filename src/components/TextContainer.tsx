c7f720a0daf7e9e4cddcb0f1343ec06c9d0172cc
Agacy junior <juniormartinssom@gmail.com>
2022-03-12 13:59:03 -0300

    Component Text finish

---
+++ src/components/TextContainer.tsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "variants";
+
+const TextContainer = () => {
+  return (
+    <motion.div variants={staggerContainer} initial="initial" animate="animate" className="textContainer">
+
+      <div className="textContainer-top">
+        <motion.span variants={fadeIn()} className="text-green-600 ">
+          Encontre seu suco natural
+        </motion.span>
+        <motion.span variants={fadeIn()} className="first-letter:text-5xl">
+          2 /4
+        </motion.span>
+      </div>
+ 
+      <div className="textContainer-middle">
+        <motion.span variants={fadeIn()} initial="initial" animate="animate">
+          Laranja
+        </motion.span>
+      </div>
+
+      <div className="textContainer-bottom ">
+        <motion.button variants={fadeIn()}>Mostrar todos os sucos</motion.button>
+        <motion.p variants={fadeIn()}>
+          Sua vida<span> saudável </span> <br />  começa aqui conosco!
+        </motion.p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default TextContainer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
 import Header from "../components/Header";
+import TextContainer from "../components/TextContainer";
+import { motion } from "framer-motion";
+import { bottle, bottleWrapper, leavesContainer } from "variants";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* texts wrapper */}
+        <TextContainer />
+
+        {/* juice bottle image */}
+        <motion.div variants={bottleWrapper} initial="initial" animate="animate" className="bottleWrapper">
+          <motion.img src="/images/juice.png" variants={bottle} className="bottle" />
+        </motion.div>
+
+
+      </main>
+    </div>
+  );
 }
